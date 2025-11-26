ccd61add79416055b226a2ecc09500cccad0338d
Caleb Pittman <ambassadoor.dev@gmail.com>
2025-11-26 11:43:19 -0600

    Added basic routes with dummy data

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Outlet, Route, Routes } from 'react-router-dom'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  const user = {}
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Routes>
+      <Route path="/" element={<><Outlet/></>}>
+        <Route index element={
+          user ? <>Login</> : <>Dashboard</>
+        }/>
+        <Route path="clubs">
+          <Route index element={<></>}/>
+          <Route path=":clubId" element={<></>}/>
+          <Route path=":userId" element={<></>}/>
+          <Route path="newClub" element={<>NewClubForm</>}/>
+          <Route path="deleteClub" element={<>Club Delete</>}/>
+        </Route>
+        <Route path="library/userId">
+          <Route index element={<></>}/>
+        </Route>
+        <Route path="book:bookId" element={<>Book</>}/>
+      </Route>
+    </Routes>
   )
 }
 
