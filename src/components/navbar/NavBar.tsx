050e03d7e2ee2e10a23775667f670a92f6cd9d46
Caleb Pittman <ambassadoor.dev@gmail.com>
2025-12-02 20:51:53 -0600

    cleaned up imports

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -2,17 +2,14 @@ import {
   AppBar,
   Box,
   Button,
-  IconButton,
-  Menu,
-  MenuItem,
   Toolbar,
   Typography,
 } from "@mui/material";
-import MenuIcon from "@mui/icons-material/Menu";
-import { AccountCircle, Book } from "@mui/icons-material";
-import { useState, type JSX } from "react";
+
+import { Book } from "@mui/icons-material";
+import { type JSX } from "react";
 import { useNavigate } from "react-router-dom";
-import { useSessionManager } from "../../hooks/useSessionManager";
+
 import { NavMenu } from "./NavMenu";
 
 type NavBarProps = {
@@ -21,9 +18,6 @@ type NavBarProps = {
 }
 
 export const NavBar = ({user, setUser}:NavBarProps): JSX.Element => {
-  const [anchorEl, setAnchorEl] = useState<null | Element>(null);
-  const [profileAnchorEl, setProfileAnchorEl] = useState<null | Element>(null);
-  const {logout} = useSessionManager()
 
   const pages = [
     {
@@ -45,22 +39,6 @@ export const NavBar = ({user, setUser}:NavBarProps): JSX.Element => {
   const handleNavLinkClick = (path: string) => {
     navigate(path);
   };
-
-  const handleOpenMenu = (event: React.MouseEvent) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleCloseMenu = () => {
-    setAnchorEl(null);
-  };
-
-  const handleOpenProfileMenu = (event: React.MouseEvent) => {
-    setProfileAnchorEl(event.currentTarget);
-  }
-
-  const handleCloseProfileMenu = () => {
-    setProfileAnchorEl(null);
-  }
 
   return (
     <Box className="bg-primary"sx={{ flexGrow: 0 }}>
@@ -86,84 +64,6 @@ export const NavBar = ({user, setUser}:NavBarProps): JSX.Element => {
               ))}
             </Box>
           </Box>
-          {/* <Box className="flex md:hidden">
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              onClick={handleOpenMenu}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              className="mt-12"
-              id="menu-appbar"
-              anchorEl={anchorEl}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              open={Boolean(anchorEl)}
-              onClose={handleCloseMenu}
-            >
-              <MenuItem onClick={() => navigate("profile")}>
-                <Typography>Profile</Typography>
-              </MenuItem>
-              {pages.map((page) => (
-                <MenuItem
-                  key={page.text}
-                  onClick={() => handleNavLinkClick(page.path)}
-                >
-                  <Typography>{page.text}</Typography>
-                </MenuItem>
-              ))}
-              {user && <MenuItem onClick={() => {
-                  logout()
-                  setUser(null)
-                  handleCloseMenu()
-                  }}>
-                  <Typography>Sign Out</Typography>
-              </MenuItem>}
-            </Menu>
-          </Box>
-          <Box className="hidden md:flex">
-            <IconButton size="large" color="inherit" onClick={handleOpenProfileMenu}>
-              <AccountCircle />
-            </IconButton>
-            <Menu
-              className="mt-12"
-              id="profile-appbar"
-              anchorEl={profileAnchorEl}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right"
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              open={Boolean(profileAnchorEl)}
-              onClose={handleCloseProfileMenu}
-            >
-              <MenuItem onClick={() => navigate("/profile")}>
-                <Typography>Profile</Typography>
-              </MenuItem>
-              {user ? <MenuItem onClick={() => {
-                  logout()
-                  setUser(null)
-                  handleCloseProfileMenu()
-                  }}>
-                  <Typography>Sign Out</Typography>
-              </MenuItem>: <MenuItem onClick={() => navigate("/signin")}><Typography>Sign In</Typography></MenuItem>}
-            </Menu>
-          </Box> */}
           <NavMenu user={user} setUser={setUser} />
         </Toolbar>
       </AppBar>
