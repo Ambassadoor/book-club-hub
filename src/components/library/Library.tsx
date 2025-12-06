659db0e0a9e79271c2bcdfbe8f773a78b7fe68b4
Caleb Pittman <ambassadoor.dev@gmail.com>
2025-12-06 09:36:04 -0600

    Added pagination control on serach results

--- src/components/library/Library.tsx
+++ src/components/library/Library.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import { getLibrary } from "../../services/libraryServices/libraryServices"
-import { TextField } from "@mui/material"
+import { Box, Button, TextField } from "@mui/material"
 import { googleBooksClient } from "../../api/googleBooksClient"
 
 export interface SearchParams {
@@ -19,6 +19,7 @@ export const Library = () => {
     const [library, setLibrary] = useState<UserBook[]>([])
     const [searchParams, setSearchParams] = useState<SearchParams>({})
     const [searchResults, setSearchResults] = useState<GoogleBook[]>([])
+    const [numResults, setNumResults] = useState<number>(0)
     const [page, setPage] = useState<number>(0)
     const {search} = googleBooksClient()
 1
@@ -29,15 +30,29 @@ export const Library = () => {
 
     useEffect(() => {
         Object.values(searchParams).length > 0 &&
-        search(searchParams, page).then(res => setSearchResults(res.items))
+        search(searchParams, page).then(
+            res => {
+                setNumResults(Number(res.totalItems))
+                setSearchResults(res.items)
+            })
 
     },[searchParams, page])
 
     const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setPage(0)
         const field = e.target.name
         const term = e.target.value
 
         setSearchParams({...searchParams, [field]: term}) 
+    }
+
+    const handleNext = () => {
+        setPage(page + 10)
+    }
+
+    const handlePrev = () => {
+        page > 0 &&
+        setPage(page - 10)
     }
 
     return (
@@ -48,11 +63,15 @@ export const Library = () => {
                     <li key={book.id}>{book.volumeInfo.title}</li>
                 ))}
             </ul>
+            <Box>
+                <Button disabled={page === 0} onClick={handlePrev}>{"<"}</Button><Button disabled={!(page + searchResults.length < numResults)} onClick={handleNext}>{">"}</Button>
+            </Box>
             <ul>
                 {library.map(book => (
                     <li key={book.id} >{book.title}</li>
                 ))}
             </ul>
+
         </>
     )
 }
